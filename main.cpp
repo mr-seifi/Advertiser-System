8491c1c4b8a2869a91157cc10c1f971e5cba4c35
Amin_Seifi <aminseiifi@gmail.com>
2021-12-22 23:08:07 +0330

    Update Main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "BaseAdvertising.h"
+#include "Advertiser.h"
 
 using namespace std;
 
 int main() {
 
+    cout << "------------ Test BaseAdvertising ------------" << endl;
     BaseAdvertising baseAdvertising1, baseAdvertising2;
     baseAdvertising1.incClicks();
     baseAdvertising1.incClicks();
@@ -18,6 +20,19 @@ int main() {
     baseAdvertising2.incClicks();
     baseAdvertising2.incClicks();
     cout << baseAdvertising1 << baseAdvertising2 << endl;
+
+    cout << "------------ Test Advertiser ------------" << endl;
+    Advertiser advertiser1, advertiser2;
+    advertiser1.setName("Amin");
+    advertiser2.setName("Hassan");
+    advertiser1.incClicks();
+    advertiser1.incClicks();
+    advertiser1.incClicks();
+    advertiser1.incClicks();
+    advertiser2.incClicks();
+    advertiser2.incClicks();
+    cout << advertiser1 << advertiser2;
+    cout << "Total Clicks: " << Advertiser::getTotalClicks() << endl;
 
     return 0;
 }
